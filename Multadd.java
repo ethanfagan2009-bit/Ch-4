0a6c3be5c9a14961d0a1332a52d05c34ae46c921
ethanfagan2009-bit <ethanfagan2009@gmail.com>
2026-09-24 14:48:04 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,22 @@
+public class Multadd {
+	
+	public static double multadd(double a, double b, double c) {
+		return a * c + b;
+	}
+	public static double multTrig(double sinx, double cosx) {
+		return multadd(1.0, Math.sin(sinx), Math.cos(cosx)/2);
+	}
+	public static double multLog(double log1, double log2) {
+		return multadd(1.0, Math.log(log1), Math.log(log2));
+	}
+	public static double expSum(double x) {
+		return multadd(x, Math.pow(Math.E, -1*x), Math.sqrt(1-Math.pow(Math.E, -1*x)));
+	}
+	
+	public static void main(String[] arg) {
+		
+		System.out.println(expSum(10));
+		System.out.println(multTrig(Math.PI/4, Math.PI/4));
+		System.out.println(multLog(10, 20));
+	}
+}
